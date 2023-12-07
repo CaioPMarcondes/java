ed4332c24156f7590e860ea91d15740c77ae5c6d
CaioPMarcondes <caiopravatta@gmail.com>
2023-12-07 18:32:18 -0300

    app java

---
+++ src/main/java/br/com/fatec/hellospringboot/resources/ClientController.java
@@ -0,0 +1,47 @@
+package br.com.fatec.hellospringboot.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fatec.hellospringboot.entities.Client;
+import br.com.fatec.hellospringboot.repositories.ClientRepository;
+import br.com.fatec.hellospringboot.services.ClientService;
+
+@RestController
+public class ClientController {
+    
+    @Autowired
+    private ClientRepository repository;
+    
+    @Autowired
+    private ClientService service;
+
+
+    @GetMapping("clients")
+    public ResponseEntity<List<Client>> getClients(){
+        return ResponseEntity.ok(service.getClients());
+    }
+    
+    @GetMapping("clients/{id}")
+    public ResponseEntity <Client> getClientById(@PathVariable int id){
+        return ResponseEntity.ok(service.getClientByID(id));
+    }
+    @DeleteMapping ("clients/{id}")
+    public ResponseEntity<Void> deleteClient(@PathVariable int id){
+        this.service.deleteClientById(id);
+        return ResponseEntity.noContent().build();
+    }
+    }
+    
+
+
+
+
+
+
